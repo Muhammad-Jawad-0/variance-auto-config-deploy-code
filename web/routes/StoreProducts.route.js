8e4186e13784f2d9daa066f346a998f8fadffc45
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2026-05-21 17:50:05 +0500

    commit code

--- web/routes/StoreProducts.route.js
+++ web/routes/StoreProducts.route.js
@@ -7,7 +7,6 @@ const ConfiguratorRoute = express.Router();
 ConfiguratorRoute.post("/save-selected-products", saveProductId);
 ConfiguratorRoute.get("/getAllStoreProducts", getAllStoreProducts);
 ConfiguratorRoute.get("/check-product-extension", checkProductExtension);
-ConfiguratorRoute.post("/cart/add-configured-item", addConfiguredProductToCart);
-// ConfiguratorRoute.post("/create-draft-order", createDraftOrder);
+ConfiguratorRoute.post("/cart/add-configured-item", addConfiguredProductToCart); 
 
 export default ConfiguratorRoute;
